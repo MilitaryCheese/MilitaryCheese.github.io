e20f3f81000cb0b89f26206e4d4b68e2790a606c
Keshini Jayasinghe <keshinijayasinghe@Keshinis-MacBook-Pro.local>
2019-12-28 12:36:27 +0530

    init commit

---
+++ src/js/scripts.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//-------------------------------*
+// JS Features for UI Commpoents
+//-------------------------------*
+
+//MDC Tabs
+var tabBar = new mdc.tabBar.MDCTabBar(document.querySelector('.mdc-tab-bar'));
+var contentEls = document.querySelectorAll('.content');
+
+tabBar.listen('MDCTabBar:activated', function (event) {
+  // Hide currently-active content
+  document.querySelector('.content--active').classList.remove('content--active');
+  // Show content for newly-activated tab
+  contentEls[event.detail.index].classList.add('content--active');
+});
+
+//MDC Input Fields
+var textFieldLocation = new mdc.textField.MDCTextField.attachTo(document.querySelector('.mdc-text-field-location'));
+var textFieldDate = new mdc.textField.MDCTextField.attachTo(document.querySelector('.mdc-text-field-date'));
