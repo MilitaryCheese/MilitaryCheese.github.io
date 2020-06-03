e6985969039393d120cc70a3e1b12c8a8c641c26
Keshini Jayasinghe <keshinijayasinghe@Keshinis-MacBook-Pro.local>
2020-06-03 16:40:00 +0530

    all changes

---
+++ index.js
@@ -0,0 +1,98 @@
+/* -----------------------------------------
+  Have focus outline only for keyboard users 
+ ---------------------------------------- */
+
+const handleFirstTab = (e) => {
+  if (e.key === "Tab") {
+    document.body.classList.add("user-is-tabbing");
+
+    window.removeEventListener("keydown", handleFirstTab);
+    window.addEventListener("mousedown", handleMouseDownOnce);
+  }
+};
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove("user-is-tabbing");
+
+  window.removeEventListener("mousedown", handleMouseDownOnce);
+  window.addEventListener("keydown", handleFirstTab);
+};
+
+window.addEventListener("keydown", handleFirstTab);
+
+const backToTopButton = document.querySelector(".back-to-top");
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered ? "scale(1)" : "scale(0)";
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
+
+// $(document).ready(function () {
+//   $("#exp-1").click(function () {
+//     $(".expandable-1").toggle();
+//   });
+// });
+
+function exp1Click() {
+  document.getElementById("expandable-1").classList.toggle("expandable-1");
+  if (document.getElementById("exp-1-btn").innerHTML == "Show More") {
+    document.getElementById("exp-1-btn").innerHTML = "Show Less";
+  } else {
+    document.getElementById("exp-1-btn").innerHTML = "Show More";
+  }
+}
+
+function exp2Click() {
+  document.getElementById("expandable-2").classList.toggle("expandable-2");
+  if (document.getElementById("exp-2-btn").innerHTML == "Show More") {
+    document.getElementById("exp-2-btn").innerHTML = "Show Less";
+  } else {
+    document.getElementById("exp-2-btn").innerHTML = "Show More";
+  }
+}
+
+//~~~~~~~~~~~~~
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
