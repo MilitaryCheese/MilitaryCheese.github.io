744f95534205fb7be001da7e0ee06cd58d455e80
Keshini Jayasinghe <keshinijayasinghe@Keshinis-MacBook-Pro.local>
2020-06-08 10:53:09 +0530

    portfolio update

--- index.js
+++ index.js
@@ -20,25 +20,6 @@ const handleMouseDownOnce = () => {
 
 window.addEventListener("keydown", handleFirstTab);
 
-const backToTopButton = document.querySelector(".back-to-top");
-let isBackToTopRendered = false;
-
-let alterStyles = (isBackToTopRendered) => {
-  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
-  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-  backToTopButton.style.transform = isBackToTopRendered ? "scale(1)" : "scale(0)";
-};
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 700) {
-    isBackToTopRendered = true;
-    alterStyles(isBackToTopRendered);
-  } else {
-    isBackToTopRendered = false;
-    alterStyles(isBackToTopRendered);
-  }
-});
-
 // $(document).ready(function () {
 //   $("#exp-1").click(function () {
 //     $(".expandable-1").toggle();
@@ -65,22 +46,22 @@ function exp2Click() {
 
 //~~~~~~~~~~~~~
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex, slideType, dotType);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides((slideIndex += n));
+function plusSlides(n, slideType, dotType) {
+  showSlides((slideIndex += n), slideType, dotType);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides((slideIndex = n));
+function currentSlide(n, slideType, dotType) {
+  showSlides((slideIndex = n), slideType, dotType);
 }
 
-function showSlides(n) {
+function showSlides(n, slideType, dotType) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName(slideType);
+  var dots = document.getElementsByClassName(slideType);
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -95,4 +76,28 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
+}
+
+function loadAll() {
+  var namelist = [
+    "winter.png",
+    "Hello Dribbble ET.jpg",
+    "Hourglass.png",
+    "INSL@2x-100.jpg",
+    "Moon Silhouette.png",
+    "z1.png",
+    "Woman 2.jpg",
+    "bikewallthingy@2x-100.jpg",
+    "jpj.jpg",
+  ];
+
+  var port = document.getElementById("portfolioSec");
+
+  for (var i = 0; i < namelist.length; i++) {
+    var wrapper = document.createElement("div");
+    var src = "./images/portfolio/" + namelist[i];
+    wrapper.className = "gallery-item";
+    wrapper.innerHTML = "<img class='gallery-image' src='" + src + "' />";
+    port.appendChild(wrapper);
+  }
 }
